ccd85f6822a6389b5bac2d66ebeea3aad00b60a2
Naheel-Azawy <naheelazawy@gmail.com>
2019-06-18 00:18:15 +0300

    quick update

--- update-configs.sh
+++ update-configs.sh
@@ -12,5 +12,6 @@ mkdir -p "$H/.config/i3"
 eval "$D/configs/i3.conf.gen.sh $H"
 mkdir -p "$H/.config/i3blocks"
 eval "$D/configs/i3blocks.conf.gen.sh $H"
+eval "$D/configs/onboard-gsettings.sh"
 
 eval "$D/link.sh" "$D" "$U" "$H"
