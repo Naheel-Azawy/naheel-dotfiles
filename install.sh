51d595f3748b393ec7cf2be036fe1d5bb88c8a9a
Naheel-Azawy <naheelazawy@gmail.com>
2020-06-15 22:27:57 +0300

    quick update

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 CMD='cd && rm -rf naheel-dotfiles-master && curl -sL https://github.com/Naheel-Azawy/naheel-dotfiles/archive/master.tar.gz | tar xz && mv naheel-dotfiles-master .dotfiles && cd .dotfiles && ./install.sh'
 
 if [ "$1" = '--docker' ]; then
+    echo 'Creating a docker image...'
     exec docker run --name arch-naheel -it archlinux sh -c "$CMD --quick base && sudo -u main sh -c 'cd && . ~/.profile && tmux'"
 elif [ -f ./scripts/ndots ]; then
     exec ./scripts/ndots install dots "$@"
