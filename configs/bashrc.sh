0377d17c9f67a2062c4ed68218621083ad06bbaf
Naheel-Azawy <naheelazawy@gmail.com>
2020-09-30 21:02:41 +0300

    quick update

--- configs/bashrc.sh
+++ configs/bashrc.sh
@@ -22,3 +22,7 @@ try_export() {   [ -d "$2" ] && export "$1=$2";:;       }
 try_add_path() { [ -d "$1" ] && export PATH="$PATH:$1";:; }
 test -f "$DOTFILES_DIR/configs/more-paths.sh" &&
     source "$DOTFILES_DIR/configs/more-paths.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
