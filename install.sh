0b6cee3a05d6ed2abc6af27e9176ceacf919b8a9
Naheel-Azawy <naheelazawy@gmail.com>
2020-10-09 18:08:05 +0300

    quick update

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-command -v curl >/dev/null || command -v apt >/dev/null && sudo apt install --yes curl
+command -v curl >/dev/null || {
+    command -v apt >/dev/null && sudo apt install --yes curl
+}
 
 CMD='cd && rm -rf naheel-dotfiles-master && curl -sL https://github.com/Naheel-Azawy/naheel-dotfiles/archive/master.tar.gz | tar xz && mv naheel-dotfiles-master .dotfiles && cd .dotfiles && ./install.sh'
 
