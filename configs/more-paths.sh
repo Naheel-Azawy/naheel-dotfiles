1f69e601a7bad71fe55cba31976806573b124bc0
Naheel-Azawy <naheelazawy@gmail.com>
2021-07-18 18:48:44 +0300

    quick update

--- configs/more-paths.sh
+++ configs/more-paths.sh
@@ -3,8 +3,8 @@
 # sourced by shells to export personal additional paths
 # `try_export` and `try_add_path` need to be defined per shell
 
-# GO ########################################
 try_add_path "$HOME/go/bin"
+try_add_path "$HOME/.cargo/bin"
 
 # ANDROID ###################################
 # TODO: move to personal
