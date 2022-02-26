0b30efeb976bfba7850ad75046155aeb796f084e
Naheel-Azawy <naheelazawy@gmail.com>
2022-02-27 01:19:16 +0300

    quick update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM archlinux:latest
 LABEL maintainer='Naheel-Azawy'
-RUN cd && rm -rf naheel-dotfiles-master && curl -sL https://github.com/Naheel-Azawy/naheel-dotfiles/archive/master.tar.gz | tar xz && mv naheel-dotfiles-master .dotfiles && cd .dotfiles && ./install.sh --quick base
-CMD sudo -u main sh -c 'cd && . ~/.profile && tmux'
+COPY . /root/.dotfiles
+RUN cd ~/.dotfiles && ./install.sh --quick base
+CMD sudo -u me sh -c 'cd && . ~/.profile && tmux'
--- install.sh
+++ install.sh
@@ -4,18 +4,8 @@ command -v curl >/dev/null || {
     command -v apt >/dev/null && sudo apt install --yes curl
 }
 
-CMD='cd && rm -rf naheel-dotfiles-master && curl -sL https://github.com/Naheel-Azawy/naheel-dotfiles/archive/master.tar.gz | tar xz && mv naheel-dotfiles-master .dotfiles && cd .dotfiles && ./install.sh'
-
 if [ "$1" = '--docker' ]; then
-
     IMG='arch-naheel'
-
-    echo 'Creating Dockerfile...'
-    echo "FROM archlinux:latest
-LABEL maintainer='Naheel-Azawy'
-RUN $CMD --quick base
-CMD sudo -u main sh -c 'cd && . ~/.profile && tmux'" > ./Dockerfile
-
     if docker images | cut -d ' ' -f 1 | grep "$IMG" -q; then
         echo "Image '$IMG' already exist"
         echo "remove with 'docker rmi $IMG'"
@@ -23,17 +13,19 @@ CMD sudo -u main sh -c 'cd && . ~/.profile && tmux'" > ./Dockerfile
         echo "Building image ($IMG)..."
         docker build -t "$IMG" .
     fi
-
     echo 'Running docker image...'
     docker run -it --rm "$IMG"
 
 elif [ -f ./scripts/nd ]; then
-
     exec ./scripts/nd install dots "$@"
 
 else
-
     echo 'Downloading dotfiles...'
-    eval "$CMD" "$@"
+    cd && rm -rf naheel-dotfiles-master &&
+        curl -sL https://github.com/Naheel-Azawy/naheel-dotfiles/archive/master.tar.gz |
+            tar xz &&
+        mv naheel-dotfiles-master .dotfiles &&
+        cd .dotfiles &&
+        ./install.sh "$@"
 
 fi
