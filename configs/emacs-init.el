e04464451904cb383684011723c61a98903e1d89
Naheel-Azawy <naheelazawy@gmail.com>
2022-05-31 19:56:49 +0300

    quick update

--- configs/emacs-init.el
+++ configs/emacs-init.el
@@ -51,7 +51,8 @@
 (global-set-key (kbd "C-a") 'mark-whole-buffer)
 (with-eval-after-load 'org
   (define-key org-mode-map "\C-a" 'mark-whole-buffer))
-(global-set-key (kbd "C-f") 'isearch-forward)
+(global-set-key (kbd "C-f") 'isearch-forward-regexp)
+(global-set-key (kbd "C-M-f") 'isearch-forward)
 (define-key isearch-mode-map "\C-f" 'isearch-repeat-forward)
 (define-key isearch-mode-map "\C-v" 'isearch-yank-pop)
 (global-set-key (kbd "C-s") 'save-buffer)
